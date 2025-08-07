8f2c5cafa326e45da3e5ac7f01143cddb469f511
Gopicharan777 <2300031500@kluniversity.in>
2025-08-07 12:21:23 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -32,11 +32,4 @@ public class AppController {
 	public String update(@RequestBody Product p) {
 		return s.updateData(p);
 	}
-
-	/* 
-	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable int id) {
-		return s.deleteData(id);
-	}
-	*/
 }
